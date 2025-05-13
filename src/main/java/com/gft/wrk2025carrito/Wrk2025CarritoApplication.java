f2837832a68d6974f71a8b2cc112b64d86a46e28
cesargv05 <crgs@gft.com>
2025-05-13 14:38:36 +0200

    Primer commit

---
+++ src/main/java/com/gft/wrk2025carrito/Wrk2025CarritoApplication.java
@@ -0,0 +1,13 @@
+package com.gft.wrk2025carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wrk2025CarritoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wrk2025CarritoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gft/wrk2025carrito/Wrk2025CarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.wrk2025carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wrk2025CarritoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
