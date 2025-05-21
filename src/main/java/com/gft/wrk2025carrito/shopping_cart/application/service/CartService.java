a30a0eb214f1a42c0c45c50de3d2bc4b7370385c
meug <meug@gft.com>
2025-05-21 11:44:24 +0200

    estructura de proyecto 2

---
+++ src/main/java/com/gft/wrk2025carrito/shopping_cart/application/service/CartService.java
@@ -0,0 +1,4 @@
+package com.gft.wrk2025carrito.shopping_cart.application.service;
+
+public interface CartService {
+}
---
+++ src/main/java/com/gft/wrk2025carrito/shopping_cart/application/service/impl/CartServiceImpl.java
@@ -0,0 +1,19 @@
+package com.gft.wrk2025carrito.shopping_cart.application.service.impl;
+
+import com.gft.wrk2025carrito.shopping_cart.application.service.CartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService {
+
+//    private CartRepository cartRepository;
+//    // private CartFactory cartFactory;
+//
+//    public CartServiceImpl(CartRepository cartRepository) {
+//        this.cartRepository = cartRepository;
+////        this.cartFactory = cartFactory
+//    }
+
+    /// aqui se hace todo
+
+}
