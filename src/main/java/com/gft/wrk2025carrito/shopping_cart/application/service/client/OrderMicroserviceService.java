25fc29f086d8c9ea5aa5847b8f8f7c0fbae9179a
cesargv05 <crgs@gft.com>
2025-06-06 09:14:39 +0200

    Update cart state fix offers request

--- src/main/java/com/gft/wrk2025carrito/shopping_cart/application/service/client/OrderMicroserviceService.java
+++ src/main/java/com/gft/wrk2025carrito/shopping_cart/application/service/client/OrderMicroserviceService.java
@@ -1,7 +1,5 @@
 package com.gft.wrk2025carrito.shopping_cart.application.service.client;
 
-import com.gft.wrk2025carrito.shopping_cart.domain.model.cart.Cart;
-
 import java.util.List;
 import java.util.Map;
 
